824a443119735c679e84c1f2666db1eb3ab8bf1c
quangnamttp <quangnamttp@gmail.com>
2025-07-25 19:59:40 +0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN,
+  VERIFY_TOKEN: process.env.VERIFY_TOKEN,
+  USER_ID: process.env.USER_ID,
+  MONGO_URI: process.env.MONGO_URI,
+  TIMEZONE: 'Asia/Ho_Chi_Minh',
+  FOREX_URL: 'https://www.forexfactory.com/calendar'
+};
