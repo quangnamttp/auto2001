b6c3dd12756d7762e833e397e1b7620d32b78a22
quangnamttp <quangnamttp@gmail.com>
2025-07-25 20:59:48 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 module.exports = {
   PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN,
   VERIFY_TOKEN: process.env.VERIFY_TOKEN,
-  USER_ID: process.env.USER_ID,
-  MONGO_URI: process.env.MONGO_URI,
-  TIMEZONE: 'Asia/Ho_Chi_Minh',
-  FOREX_URL: 'https://www.forexfactory.com/calendar'
+  USER_ID: process.env.USER_ID
 };
